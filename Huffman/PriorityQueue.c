1aeef7ae9f076214da281f743311549ab6f99e44
YodaDevs <37128180+YodaDevs@users.noreply.github.com>
2018-09-23 22:51:13 -0300

    Reformando PriorityQueue.c

--- Huffman/PriorityQueue.c
+++ Huffman/PriorityQueue.c
@@ -2,71 +2,84 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include "Tree.h"
 #include "PriorityQueue.h"
-#include "tree.h"
+#include "struct.h"
 
-//Make only one new priority queue
+//criando somente uma fila nova
 void* create_priority_queue()
 {
-  priority_queue *new_queue = (priority_queue*) malloc(sizeof(priority_queue));
+  PriorityQueue *new_queue = (PriorityQueue*) calloc(1,sizeof(PriorityQueue));
+  new_queue->size_node = 0;
   new_queue->head = NULL;
-  new_queue->size = 0;
-  return (priority_queue*) new_queue;
+  return (void*) new_queue;
 }
 
-// Putting item on queue
-void* enqueue(priority_queue *pq, Tree_Node *aux1)
-{
-  pq_node *new_node = (pq_node*) malloc(sizeof(pq_node));
-  pq->size++;
-  new_node->item = aux1;
-  new_node->priority = aux1->priority;
-    if ((is_empty(pq)) || (pq->head->priority >= aux1->priority)) {
+void* create_node_tree(unsigned char ch, unsigned int priority){
+  Huffman* node = (Huffman*) calloc(1,sizeof(Huffman));
+  node->priority = priority;
+  node->ch = ch;
+  node->left = node->right = node->next = NULL;
+  return (void*) node;
+}
+
+// Colocar item na fila
+void enqueue_node(PriorityQueue *pq, unsigned char ch, unsigned int priority){
+  Huffman *new_node = create_node_tree(ch,priority);
+
+    if ((is_empty(pq)) || (pq->head->priority) >= priority) {
       new_node->next = pq->head;
       pq->head = new_node;
-      return (priority_queue*) pq;
-  }
-  pq_node *aux2 = pq->head;
 
-  while(aux2->next!= NULL && aux2->next->priority < aux1->priority){
-    aux2 = aux2->next;
-  }
-  new_node->next = aux2->next;
-  aux2->next = new_node;
-  return (priority_queue*) pq;
-}
-
-//Remove the last in the queue
-void* dequeue(priority_queue *pq)
-{
-  if (is_empty(pq)) {
-    printf("Priority Queue underflow");
-    return NULL;
   } else {
-    pq->size--;
-    Tree_Node *aux = pq->head->item;
-    pq->head = pq->head->next;
-    return (Tree_Node*)aux;
+      Huffman *current = pq->head;
+    while ((current->next != NULL) && (current->next->priority < priority)) {
+      current = current->next;
+    }
+      new_node->next = current->next;
+      current->next = new_node;
   }
+  pq->size_node++;
 }
 
-//Verifying that the queue is empty
-void* is_empty(priority_queue *pq){
-  return (pq->head == NULL) ? ((int*)0) : ((int*)1);
+void enqueue_daddy_node(PriorityQueue *pq, Huffman * daddy_node){
+  if(is_empty(pq) || pq->head->priority >= daddy_node->priority){
+    daddy_node->next = pq->head;
+    pq->head = daddy_node;
+  }
+  else{
+    Huffman* current = pq->head;
+    while(current->next != NULL && current->next->priority < daddy_node->priority){
+      current = current->next;
+    }
+    daddy_node->next = current->next;
+    current->next = daddy_node;
+  }
+  pq->size_node++;
 }
 
-//Checking the size of the priority queue
-void* size_pq(priority_queue *pq){
-  return &(pq->size); //if I want to return the value in main, for example: printf("%d\n",*(int*)size_pq(pq));
-}
+//retirar o ultimo da fila
+void* dequeue(PriorityQueue *pq){
 
-//Printing the last item queue to the first
-void print_priority_queue(priority_queue *pq){
-  if(!is_empty(pq)){
-    printf("Não está vazia\n");
-    printf("prioridade: %d ", pq->head->priority);
-    printf("\n");
+  if (!(is_empty(pq))) {
+    Huffman* node = pq->head;
     pq->head = pq->head->next;
-    print_priority_queue(pq);
+    node->next = NULL;
+    return (void*) node;
+  }
+  return (void*)NULL;
+}
+
+//Verificando se a fila está vazia
+void* is_empty(PriorityQueue *pq){
+  return (pq->head == NULL) ? ((void*)1) : ((void*)0);
+}
+
+void  create_queue_array(PriorityQueue *new_queue, unsigned int str[]){
+  int a;
+  for(a=0; a < 256; a++){
+    if(str[a] != 0){
+      enqueue_node(new_queue,a,str[a]);
+    }
   }
 }
